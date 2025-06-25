6be04171dc8cc9f8e70879a79f535f833bc0cdc2
Stan Yan <stanhavenoidea@gmail.com>
2025-06-25 12:27:53 +1000

    init

---
+++ components/home/Citys.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Citys = () => {
+  return <div>Citys</div>;
+};
+
+export default Citys;
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-amber-300 underline">adasf</h1>
+    </>
+  );
+}
+
+export default App;
